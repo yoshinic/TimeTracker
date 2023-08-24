b10e957d3ecf00de82bab853534ae558b6889bfa
yoshi <s.y.nakada@gmail.com>
2023-08-25 00:59:45 +0900

    Initial Commit

---
+++ TimeTracker/TimeTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTrackerApp.swift
+//  TimeTracker
+//
+//  Created by YOSHI on 2023/08/25.
+//
+
+import SwiftUI
+
+@main
+struct TimeTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
