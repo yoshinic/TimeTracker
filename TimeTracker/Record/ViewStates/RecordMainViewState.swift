3521623a5f011a9f6fc0b5719b17a74521153d5c
yoshi <s.y.nakada@gmail.com>
2023-11-07 12:43:41 +0900

    追加：レコード表示切替 Radio Button

--- TimeTracker/Record/ViewStates/RecordMainViewState.swift
+++ TimeTracker/Record/ViewStates/RecordMainViewState.swift
@@ -1,4 +1,3 @@
-import Foundation
 import Combine
 import TimeTrackerAPI
 
@@ -8,9 +7,20 @@ class RecordMainViewState: ObservableObject {
     @Published var showListView: Bool = true
     @Published var isEditMode: Bool = false
 
+    let titles: [String] = ["リスト", "チャート"]
+
     init() {}
 
     func onTapCompleteButton() {
         isEditMode.toggle()
+    }
+
+    func onTapRadioButton(old: Int, new: Int) {
+        guard old != new else { return }
+        switch new {
+        case 0: showListView = true
+        case 1: showListView = false
+        default: return
+        }
     }
 }
--- TimeTracker/Record/Views/RecordMainView.swift
+++ TimeTracker/Record/Views/RecordMainView.swift
@@ -6,6 +6,18 @@ struct RecordMainView: View {
     var body: some View {
         Form {
             SearchRecordView(state: .init())
+            Section("") {
+                HStack {
+                    SearchRecordTitleView("表示切替")
+                    Spacer()
+                    RadioButton(state: .init(
+                        selectedIdx: state.showListView ? 0 : 1,
+                        titles: state.titles,
+                        onTapItem: state.onTapRadioButton
+                    ))
+                }
+            }
+
             if state.showListView {
                 RecordListView(state: .init())
             } else {
